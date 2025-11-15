a1f358a10d2ed6fcd915f73ad33660461d0c217a
Reyhaneh Ebrahimi <abrahimireyhane@gmail.com>
2025-11-15 23:23:41 +0330

    Add files via upload

---
+++ ANIMALS.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal frog = new Frog();
+            Animal cat = new Cat();
+            Animal dog = new Dog();
+            Animal crow = new Crow();
+            frog.MakeSound();
+            frog.Eat();
+            frog.Walk();
+            frog.Sleep();
+
+            Console.WriteLine("\n---FROG---");
+
+
+            cat.MakeSound();
+            cat.Eat();
+            cat.Walk();
+            cat.Sleep();
+
+            Console.WriteLine("\n---CAT---");
+
+            
+            dog.MakeSound();
+            dog.Eat();
+            dog.Walk();
+            dog.Sleep();
+            Console.WriteLine("\n---DOG---");
+
+            
+            crow.MakeSound();
+            crow.Eat();
+            crow.Walk();
+            crow.Sleep();
+            Console.WriteLine("\n---CROW---");
+            
+          
+    
+    Console.ReadKey();
+            
+
+
+        }
+    }
+}
